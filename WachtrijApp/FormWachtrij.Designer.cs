4a96f0b0495b1164c345660539660c936e1a2242
toonvanderlely <75312183+toonvanderlely@users.noreply.github.com>
2021-02-23 09:40:26 +0100

    Initial commit

---
+++ WachtrijApp/FormWachtrij.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WachtrijApp
+{
+    partial class FormWachtrij
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormWachtrij));
+            this.labelTitel = new System.Windows.Forms.Label();
+            this.labelWachttijd = new System.Windows.Forms.Label();
+            this.labelWachttijdMelding = new System.Windows.Forms.Label();
+            this.AttractieLogo = new System.Windows.Forms.PictureBox();
+            this.labelKar2 = new System.Windows.Forms.Label();
+            this.labelKar1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.AttractieLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTitel
+            // 
+            this.labelTitel.AutoSize = true;
+            this.labelTitel.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitel.Location = new System.Drawing.Point(26, 18);
+            this.labelTitel.Name = "labelTitel";
+            this.labelTitel.Size = new System.Drawing.Size(186, 32);
+            this.labelTitel.TabIndex = 0;
+            this.labelTitel.Text = "Bugs of Horror";
+            // 
+            // labelWachttijd
+            // 
+            this.labelWachttijd.AutoSize = true;
+            this.labelWachttijd.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWachttijd.Location = new System.Drawing.Point(27, 149);
+            this.labelWachttijd.Name = "labelWachttijd";
+            this.labelWachttijd.Size = new System.Drawing.Size(99, 25);
+            this.labelWachttijd.TabIndex = 4;
+            this.labelWachttijd.Text = "Wachttijd";
+            // 
+            // labelWachttijdMelding
+            // 
+            this.labelWachttijdMelding.AutoSize = true;
+            this.labelWachttijdMelding.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWachttijdMelding.Location = new System.Drawing.Point(29, 174);
+            this.labelWachttijdMelding.Name = "labelWachttijdMelding";
+            this.labelWachttijdMelding.Size = new System.Drawing.Size(71, 18);
+            this.labelWachttijdMelding.TabIndex = 5;
+            this.labelWachttijdMelding.Text = "0 minuten";
+            // 
+            // AttractieLogo
+            // 
+            this.AttractieLogo.ErrorImage = null;
+            this.AttractieLogo.Image = ((System.Drawing.Image)(resources.GetObject("AttractieLogo.Image")));
+            this.AttractieLogo.Location = new System.Drawing.Point(168, 87);
+            this.AttractieLogo.Name = "AttractieLogo";
+            this.AttractieLogo.Size = new System.Drawing.Size(110, 141);
+            this.AttractieLogo.TabIndex = 6;
+            this.AttractieLogo.TabStop = false;
+            this.AttractieLogo.Click += new System.EventHandler(this.AttractieLogo_Click);
+            // 
+            // labelKar2
+            // 
+            this.labelKar2.AutoSize = true;
+            this.labelKar2.Font = new System.Drawing.Font("Calibri", 11.25F);
+            this.labelKar2.Location = new System.Drawing.Point(29, 87);
+            this.labelKar2.Name = "labelKar2";
+            this.labelKar2.Size = new System.Drawing.Size(57, 18);
+            this.labelKar2.TabIndex = 8;
+            this.labelKar2.Text = "Kar 2: ...";
+            // 
+            // labelKar1
+            // 
+            this.labelKar1.AutoSize = true;
+            this.labelKar1.Font = new System.Drawing.Font("Calibri", 11.25F);
+            this.labelKar1.Location = new System.Drawing.Point(29, 68);
+            this.labelKar1.Name = "labelKar1";
+            this.labelKar1.Size = new System.Drawing.Size(57, 18);
+            this.labelKar1.TabIndex = 9;
+            this.labelKar1.Text = "Kar 1: ...";
+            // 
+            // FormWachtrij
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Peru;
+            this.ClientSize = new System.Drawing.Size(279, 227);
+            this.Controls.Add(this.labelKar1);
+            this.Controls.Add(this.labelKar2);
+            this.Controls.Add(this.AttractieLogo);
+            this.Controls.Add(this.labelWachttijdMelding);
+            this.Controls.Add(this.labelWachttijd);
+            this.Controls.Add(this.labelTitel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormWachtrij";
+            this.Text = "Bugs of Horror";
+            ((System.ComponentModel.ISupportInitialize)(this.AttractieLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitel;
+        private System.Windows.Forms.Label labelWachttijd;
+        private System.Windows.Forms.Label labelWachttijdMelding;
+        private System.Windows.Forms.PictureBox AttractieLogo;
+        private System.Windows.Forms.Label labelKar2;
+        private System.Windows.Forms.Label labelKar1;
+    }
+}
+
---
+++ WachtrijApp/FormWachtrij.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WachtrijApp
+{
+    public partial class FormWachtrij : Form
+    {
+        public FormWachtrij()
+        {
+            InitializeComponent();
+        }
+
+        //  Er is op het logo geklikt. Die actie start een event welke de onderstaande methode aanroept.
+        private void AttractieLogo_Click(object sender, EventArgs e)
+        {
+            VerwerkWachtrijSensorData();
+
+            VerwerkAttractieStatusData();
+        }
+
+        private void VerwerkWachtrijSensorData()
+        {
+            //  Roep de methode aan welke de wachttijd berekend.
+            int Wachttijd = BerekenWachttijd();
+
+            //  Gebruik de wachttijd om de tekst in de label 'labelWachttijdMelding' aan te passen.
+            this.labelWachttijdMelding.Text = $"{Wachttijd} minuten";
+        }
+
+        //  Deze methode leest de sensordata in het betreffende XML bestand.
+        //  Deze methode berekend vervolgens de de wachttijd in minuten en geeft deze terug.
+        private int BerekenWachttijd()
+        {
+            int Wachttijd = 0;
+
+            //  Lees het XML WachtrijSensoren bestand uit welke meet waar mensen staan te wachten.
+            XmlDocument doc = new XmlDocument();
+            doc.Load("SensorData\\WachtrijSensoren.xml");
+
+            //  Selecteer de XML node 'Sensor01' en lees vervolgens de waarde binnen het element.
+            //  Wanneer de sensor geen mensen detecteerd, geef de tot nu to berekende wachttijd terug.
+            //  Wanneer de sensor wel mensen detecteerd, bereken de nieuwe wachttijd en ga door naar de volgende sensor.
+            string node01 = doc.DocumentElement.SelectSingleNode("/Sensoren/Sensor01").InnerText;
+            if (node01 == "False")
+            {
+                return Wachttijd;
+            }
+            Wachttijd += 10;
+
+            string node02 = doc.DocumentElement.SelectSingleNode("/Sensoren/Sensor02").InnerText;
+            if (node02 == "False")
+            {
+                return Wachttijd;
+            }
+            Wachttijd += 5;
+
+            string node03 = doc.DocumentElement.SelectSingleNode("/Sensoren/Sensor03").InnerText;
+            if (node03 == "False")
+            {
+                return Wachttijd;
+            }
+            Wachttijd += 5;
+
+            string node04 = doc.DocumentElement.SelectSingleNode("/Sensoren/Sensor04").InnerText;
+            if (node04 == "False")
+            {
+                return Wachttijd;
+            }
+            Wachttijd += 5;
+
+            string node05 = doc.DocumentElement.SelectSingleNode("/Sensoren/Sensor05").InnerText;
+            if (node05 == "False")
+            {
+                return Wachttijd;
+            }
+            Wachttijd += 5;
+
+            string node06 = doc.DocumentElement.SelectSingleNode("/Sensoren/Sensor06").InnerText;
+            if (node06 == "False")
+            {
+                return Wachttijd;
+            }
+            Wachttijd += 5;
+
+            string node07 = doc.DocumentElement.SelectSingleNode("/Sensoren/Sensor07").InnerText;
+            if (node07 == "False")
+            {
+                return Wachttijd;
+            }
+            Wachttijd += 5;
+
+            string node08 = doc.DocumentElement.SelectSingleNode("/Sensoren/Sensor08").InnerText;
+            if (node08 == "False")
+            {
+                return Wachttijd;
+            }
+            Wachttijd += 5;
+
+            return Wachttijd;
+        }
+
+        private void VerwerkAttractieStatusData()
+        {
+            //  Lees het XML AttractieStatus bestand uit welke de data van de karretjes uitleest.
+            XmlDocument doc = new XmlDocument();
+            doc.Load("SensorData\\AttractieStatus.xml");
+
+            //  Selecteer de XML node 'Kar01' en lees vervolgens de waarde binnen het element.
+            //  Converteer de statuc-code in een status-beschrijving.
+            //  Gebruik de status-beschrijving om de tekst in de label 'labelKar1' aan te passen.
+            string node1 = doc.DocumentElement.SelectSingleNode("/Status/Kar01").InnerText;
+            string status1 = ConvertStatus(node1);
+            this.labelKar1.Text = $"Kar 1: {status1}";
+
+            string node2 = doc.DocumentElement.SelectSingleNode("/Status/Kar02").InnerText;
+            string status2 = ConvertStatus(node2);
+            this.labelKar2.Text = $"Kar 2: {status2}";
+        }
+
+        //  Een methode welke een status-code omzet naar een status-beschrijving
+        private string ConvertStatus(string StatusNr)
+        {
+            if (StatusNr == "1")
+            {
+                return "uit/instappen";
+            }
+
+            if (StatusNr == "2")
+            {
+                return "Klaar voor vertrek";
+            }
+
+            if (StatusNr == "3")
+            {
+                return "Op avontuur";
+            }
+
+            if (StatusNr == "4")
+            {
+                return "Komt binnen";
+            }
+
+            return "";
+        }
+    }
+}
