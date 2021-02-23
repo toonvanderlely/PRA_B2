46c53137282d9cede001447d35c574c469ec572d
AlirizaSari <72546588+AlirizaSari@users.noreply.github.com>
2021-02-23 10:41:19 +0100

    fix #2
    Heb de sensors bij gedaan en de tijd veranderd naar wat het moest zijn.

--- WachtrijApp/FormWachtrij.cs
+++ WachtrijApp/FormWachtrij.cs
@@ -46,56 +46,84 @@ namespace WachtrijApp
             {
                 return Wachttijd;
             }
-            Wachttijd += 10;
+            Wachttijd += 6;
 
             string node02 = doc.DocumentElement.SelectSingleNode("/Sensoren/Sensor02").InnerText;
             if (node02 == "False")
             {
                 return Wachttijd;
             }
-            Wachttijd += 5;
+            Wachttijd += 6;
 
             string node03 = doc.DocumentElement.SelectSingleNode("/Sensoren/Sensor03").InnerText;
             if (node03 == "False")
             {
                 return Wachttijd;
             }
-            Wachttijd += 5;
+            Wachttijd += 4;
 
             string node04 = doc.DocumentElement.SelectSingleNode("/Sensoren/Sensor04").InnerText;
             if (node04 == "False")
             {
                 return Wachttijd;
             }
-            Wachttijd += 5;
+            Wachttijd += 4;
 
             string node05 = doc.DocumentElement.SelectSingleNode("/Sensoren/Sensor05").InnerText;
             if (node05 == "False")
             {
                 return Wachttijd;
             }
-            Wachttijd += 5;
+            Wachttijd += 4;
 
             string node06 = doc.DocumentElement.SelectSingleNode("/Sensoren/Sensor06").InnerText;
             if (node06 == "False")
             {
                 return Wachttijd;
             }
-            Wachttijd += 5;
+            Wachttijd += 4;
 
             string node07 = doc.DocumentElement.SelectSingleNode("/Sensoren/Sensor07").InnerText;
             if (node07 == "False")
             {
                 return Wachttijd;
             }
-            Wachttijd += 5;
+            Wachttijd += 4;
 
             string node08 = doc.DocumentElement.SelectSingleNode("/Sensoren/Sensor08").InnerText;
             if (node08 == "False")
             {
                 return Wachttijd;
             }
-            Wachttijd += 5;
+            Wachttijd += 4;
+
+            string node09 = doc.DocumentElement.SelectSingleNode("/Sensoren/Sensor09").InnerText;
+            if (node09 == "False")
+            {
+                return Wachttijd;
+            }
+            Wachttijd += 4;
+
+            string node10 = doc.DocumentElement.SelectSingleNode("/Sensoren/Sensor10").InnerText;
+            if (node10 == "False")
+            {
+                return Wachttijd;
+            }
+            Wachttijd += 4;
+
+            string node11 = doc.DocumentElement.SelectSingleNode("/Sensoren/Sensor11").InnerText;
+            if (node11 == "False")
+            {
+                return Wachttijd;
+            }
+            Wachttijd += 4;
+
+            string node12 = doc.DocumentElement.SelectSingleNode("/Sensoren/Sensor12").InnerText;
+            if (node12 == "False")
+            {
+                return Wachttijd;
+            }
+            Wachttijd += 4;
 
             return Wachttijd;
         }
