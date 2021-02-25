a1c70e024aed90736e088f753af3f8c06f6a7b73
AlirizaSari <72546588+AlirizaSari@users.noreply.github.com>
2021-02-25 11:22:53 +0100

    Fix #3

--- WachtrijApp/FormWachtrij.cs
+++ WachtrijApp/FormWachtrij.cs
@@ -147,7 +147,7 @@ namespace WachtrijApp
 
             string node3 = doc.DocumentElement.SelectSingleNode("/Status/Kar03").InnerText;
             string status3 = ConvertStatus(node3);
-            this.labelKar3.Text = $"Kar 2: {status3}";
+            this.labelKar3.Text = $"Kar 3: {status3}";
         }
 
         //  Een methode welke een status-code omzet naar een status-beschrijving
